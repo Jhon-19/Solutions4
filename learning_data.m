df4d49a325b939ca4ef5599a4b5a20fef11f807d
Resurgence27 <1609474648@qq.com>
2020-08-20 17:22:47 +0800

    learning data

---
+++ learning_data.m
@@ -0,0 +1,22 @@
+clear;
+clc;
+data_train= dlmread("datas_all.txt");
+[num, txt, raw] = getDatas();
+%disp(num);
+
+format short
+num_result = num (:,4);
+
+
+num_train(:,1:3) = num(:,1:3);
+num_test(:,1:3) = num(:,1:3) ;
+for i = 1:1:length(num)
+    if(num_result(i) == 1)
+        num_result(i) = true;
+    else
+        num_result(i) = false;
+    end
+end
+num_test(:,3) = 70 ;
+disp(num_result);
+%disp(num_test);
